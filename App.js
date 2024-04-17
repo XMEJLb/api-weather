a918f5886333f00d16808a383a488d7d69b76f8e
Den <khmell@ya.com>
2024-04-17 22:27:46 +0300

    58

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import { Text } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { s } from "./App.style";
-
+import { Home } from "./Pages/Home/Home";
+import { ImageBackground } from "react-native";
+import backgroundImg from "./assets/background.png";
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView>
-        <Text></Text>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <ImageBackground
+      imageStyle={s.img}
+      style={s.img_background}
+      source={backgroundImg}
+    >
+      <SafeAreaProvider>
+        <SafeAreaView style={s.container}>
+          <Home />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ImageBackground>
   );
 }
--- App.style.js
+++ App.style.js
@@ -1,5 +1,14 @@
 import { StyleSheet } from "react-native";
 
-const s = StyleSheet.create({
-  container: {},
+export const s = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  img_background: {
+    flex: 1,
+    backgroundColor: "black",
+  },
+  img: {
+    opacity: 0.75,
+  },
 });
